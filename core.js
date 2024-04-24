626f02a30f5b9941a9a1ab35a96753286de5882a
dingcang <dingcang@outlook.com>
2024-04-24 16:19:37 +0800

    feat: 允许csdn未登录进行复制

---
+++ core.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         web helper
+// @namespace    dc
+// @version      0.0.1
+// @description  网页助手
+// @author       dc
+// @match        *://*/*
+// @run-at      document-start
+// @grant        none
+// ==/UserScript==
+
+(function() {
+  'use strict';
+  if(document.domain.includes('csdn')){
+      Object.defineProperty(window, "copyPopSwitch", {
+          value: false,
+          writable: false,
+      });
+  }
+})();
